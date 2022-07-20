834c177e05ff4dde95df2ca29313f3641186c2fe
yueyinqiu <lujiaqi2003@126.com>
2022-07-20 16:54:06 +0800

    移除四课中的第一课第二课第三课第四课

--- SixRens.Api/SixRens.Api/实体/壬式/I四课.cs
+++ SixRens.Api/SixRens.Api/实体/壬式/I四课.cs
@@ -11,10 +11,5 @@ namespace SixRens.Api.实体.壬式
         EarthlyBranch 辰 { get; }
         EarthlyBranch 辰阳 { get; }
         EarthlyBranch 辰阴 { get; }
-
-        public (HeavenlyStem 下, EarthlyBranch 上) 第一课 => (日, 日阳);
-        public (EarthlyBranch 下, EarthlyBranch 上) 第二课 => (日阳, 日阴);
-        public (EarthlyBranch 下, EarthlyBranch 上) 第三课 => (辰, 辰阳);
-        public (EarthlyBranch 下, EarthlyBranch 上) 第四课 => (辰阳, 辰阴);
     }
 }
