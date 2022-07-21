1b7a373ae4cac49ae85fef18254fa519e395f40c
yueyinqiu <lujiaqi2003@126.com>
2022-07-21 20:37:14 +0800

    添加四时局

--- SixRens.Api/SixRens.Api/工具/地支四时扩展.cs
+++ SixRens.Api/SixRens.Api/工具/地支四时扩展.cs
@@ -1,4 +1,6 @@
-﻿using YiJingFramework.StemsAndBranches;
+﻿using System.Collections;
+using YiJingFramework.FiveElements;
+using YiJingFramework.StemsAndBranches;
 
 namespace SixRens.Api.工具
 {
@@ -7,6 +9,51 @@ namespace SixRens.Api.工具
         public static 四时 获取四时(this EarthlyBranch 支)
         {
             return (四时)(支.Index % 12 / 4);
+        }
+
+        public static I四时局 取所在四时局(this EarthlyBranch 支)
+        {
+            return new 四时局(支);
+        }
+
+        public interface I四时局 : IEnumerable<EarthlyBranch>
+        {
+            EarthlyBranch 孟支 { get; }
+            四时 四时 => 孟支.获取四时();
+            public EarthlyBranch 仲支 => 孟支.Next();
+            public EarthlyBranch 季支 => 孟支.Next(2);
+            public FiveElement 五行 => 孟支.五行();
+            private IEnumerable<EarthlyBranch> AsEnumerable()
+            {
+                yield return 孟支;
+                yield return 仲支;
+                yield return 季支;
+            }
+
+            IEnumerator<EarthlyBranch> IEnumerable<EarthlyBranch>.GetEnumerator()
+            {
+                return AsEnumerable().GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return AsEnumerable().GetEnumerator();
+            }
+        }
+
+        private class 四时局 : I四时局
+        {
+            public EarthlyBranch 孟支 { get; }
+
+            internal 四时局(EarthlyBranch 其中一支)
+            {
+                孟支 = 其中一支.获取孟仲季() switch
+                {
+                    孟仲季.孟 => 其中一支,
+                    孟仲季.仲 => 其中一支.Next(-1),
+                    _ => 其中一支.Next(-2),
+                };
+            }
         }
     }
 }
