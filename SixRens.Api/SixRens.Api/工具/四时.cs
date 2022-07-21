e8f4c412412ff7d14fbbf9051787a5337fff2383
yueyinqiu <lujiaqi2003@126.com>
2022-07-21 20:22:29 +0800

    添加四时扩展

---
+++ SixRens.Api/SixRens.Api/工具/四时.cs
@@ -0,0 +1,10 @@
+﻿namespace SixRens.Api.工具
+{
+    public enum 四时
+    {
+        冬 = 0,
+        春 = 1,
+        夏 = 2,
+        秋 = 3,
+    }
+}
---
+++ SixRens.Api/SixRens.Api/工具/地支四时扩展.cs
@@ -0,0 +1,12 @@
+﻿using YiJingFramework.StemsAndBranches;
+
+namespace SixRens.Api.工具
+{
+    public static class 地支四时扩展
+    {
+        public static 四时 获取四时(this EarthlyBranch 支)
+        {
+            return (四时)(支.Index % 12 / 4);
+        }
+    }
+}
