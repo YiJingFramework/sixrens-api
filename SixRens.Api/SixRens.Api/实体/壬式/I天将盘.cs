5eae7e7f9a03eef985e514449d400d40071d09e6
yueyinqiu <lujiaqi2003@126.com>
2022-08-06 16:17:10 +0800

    修改去冗天将盘

--- SixRens.Api/SixRens.Api/实体/壬式/I天将盘.cs
+++ SixRens.Api/SixRens.Api/实体/壬式/I天将盘.cs
@@ -4,10 +4,12 @@ namespace SixRens.Api.实体.壬式
 {
     public interface I去冗天将盘
     {
-        天将 取乘将(EarthlyBranch 天盘支);
+        bool 为顺行 { get; }
+        EarthlyBranch 贵人天盘所乘 { get; }
     }
     public interface I天将盘 : I去冗天将盘
     {
+        天将 取乘将(EarthlyBranch 天盘支);
         EarthlyBranch 取乘神(天将 天将);
         EarthlyBranch 取临地(天将 天将);
     }
